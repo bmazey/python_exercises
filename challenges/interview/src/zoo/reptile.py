c17aa37bd282ab5d43da5b2f08dbbe059380e2bc
oliviacampili <oc2273@columbia.edu>
2018-07-18 16:44:42 -0400

    work on interview

--- challenges/interview/src/zoo/reptile.py
+++ challenges/interview/src/zoo/reptile.py
@@ -4,3 +4,12 @@ from challenges.interview.src.zoo.animal import Animal
 class Reptile(Animal):
     """this is our Reptile class"""
     # TODO - implement this class!
+    def __init__(self, name):
+        super().__init__(name)
+        self.warm_blooded = False
+
+    def is_warm_blooded(self):
+        return self.warm_blooded
+
+    def eat(self):
+        return 'crunch ... crunch ...'
