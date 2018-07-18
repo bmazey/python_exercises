9733eaaa9fa29382092001f51d149d03351e10ee
oliviacampili <oc2273@columbia.edu>
2018-07-18 16:15:14 -0400

    work on interview

--- challenges/interview/src/password_generator.py
+++ challenges/interview/src/password_generator.py
@@ -1,12 +1,22 @@
 import random
 
-
 class PasswordGenerator(object):
     """this is a low quality PasswordGenerator"""
 
     @staticmethod
     def generate_password():
         # TODO - implement this method!
-
+        letters = 'abcdefghijklmnopqrstuvwxyz'
+        numbers = '123456789'
+        symbols = '!@#$%^&*'
         password = ''
+        for i in range (5):
+            password = password + random.choice(letters)
+
+        for i in range(4):
+            password = password + random.choice(numbers)
+
+        for i in range(1):
+            password = password + random.choice(symbols)
+
         return password
