dc3658287e26e504364e8567f44cfaac0ff40808
s <shoutianze@outlook.com>
2018-07-19 03:12:28 +0800

    Update fizzbuzz

--- challenges/interview/src/fizzbuzz.py
+++ challenges/interview/src/fizzbuzz.py
@@ -6,7 +6,6 @@ class FizzBuzz(object):
     def fizzbuzz(i):
         # TODO - implement this method!
 
-
         if i%3 == 0: string = "fizz"
         if i%5 == 0: string = "buzz"
         if i%15 == 0: string = "fizzbuzz"
