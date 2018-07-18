f7ac56fc206c1056479a3d5ab2b7f2f91463c3f0
aj2852 <aj2852@columbia.edu>
2018-07-18 15:43:17 -0400

    Merge branch 'development' of C:\Users\zande\PycharmProjects\python_exercises with conflicts.

--- challenges/interview/test/test_fizzbuzz.py
+++ challenges/interview/test/test_fizzbuzz.py
@@ -1,6 +1,12 @@
 import pyprimes
 from challenges.interview.src.fizzbuzz import FizzBuzz
 
+# each number between 1 and 100 is evaluated
+for prime in range(1, 100):
+    # if the number is divided by three and has a remainder of 0, then it is a multiple of 3
+    if (prime % 3) == 0:
+
+
 
 def test_fizzbuzz():
     prime = pyprimes.primes_above(5)
