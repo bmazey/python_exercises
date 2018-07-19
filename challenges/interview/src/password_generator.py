3903ee48c454783df3d42d88618f8a73250541e5
DanBal332 <db3260@columbia.edu>
2018-07-19 15:21:37 -0400

    hi

--- challenges/interview/src/password_generator.py
+++ challenges/interview/src/password_generator.py
@@ -4,9 +4,43 @@ import random
 class PasswordGenerator(object):
     """this is a low quality PasswordGenerator"""
 
+    # TODO - implement this method!
+
     @staticmethod
     def generate_password():
-        # TODO - implement this method!
-
+        chars = 'abcdefghijklmnopqrstuvwxyz'
         password = ''
-        return password
+        for i in range(5):
+            password += random.choice(chars)
+        chars2 = '0123456789'
+        for i in range(6, 10):
+            password += random.choice(chars2)
+        chars3 = '!@#$%^&*'
+        for i in range(10, 11):
+            password += random.choice(chars3)
+        print(password)
+
+        chars = 'abcdefghijklmnopqrstuvwxyz'
+        password2 = ''
+        for i in range(5):
+            password2 += random.choice(chars)
+        chars2 = '0123456789'
+        for i in range(6, 10):
+            password2 += random.choice(chars2)
+        chars3 = '!@#$%^&*'
+        for i in range(10, 11):
+            password2 += random.choice(chars3)
+
+        if password == password2:
+            chars = 'abcdefghijklmnopqrstuvwxyz'
+        password2 = ''
+        for i in range(5):
+            password2 += random.choice(chars)
+        chars2 = '0123456789'
+        for i in range(6, 10):
+            password2 += random.choice(chars2)
+        chars3 = '!@#$%^&*'
+        for i in range(10, 11):
+            password2 += random.choice(chars3)
+            return password2
+
