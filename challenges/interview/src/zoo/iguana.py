7c7d22dc76c883a31049b1bc0a1a80ef04c77109
sophiaterenik <st3211@columbia.edu>
2018-07-19 16:04:45 -0400

    zoo

--- challenges/interview/src/zoo/iguana.py
+++ challenges/interview/src/zoo/iguana.py
@@ -1,6 +1,27 @@
 from challenges.interview.src.zoo.reptile import Reptile
 
-
 class Iguana(Reptile):
     """this is our public Iguana class, which 'extends' Reptile"""
     # TODO - implement this class!
+    def __init__(self, name):
+        super().__init__(name)
+        self.warm_blooded = False
+
+    def get_size(self):
+        return 3
+
+    def get_legs(self):
+        return 4
+
+    def get_color(self):
+        return 'green'
+
+    # examples ... using @property later
+    def is_warm_blooded(self):
+        return self.warm_blooded
+
+
+    # all mammals have specialized teeth ... they eat differently!
+    # @override
+    def eat(self):
+        return 'crunch ... crunch ...'
