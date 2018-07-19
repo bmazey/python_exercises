4246dd2da484eb92ed0bcbc6a2d6e6d82802dbda
Ferennie <ferennie.liu0119@gmail.com>
2018-07-19 15:52:33 -0400

    Zoo code with comments

--- challenges/interview/src/zoo/panther.py
+++ challenges/interview/src/zoo/panther.py
@@ -5,11 +5,16 @@ class Panther(Mammal):
     """this is our Panther class which 'extends' Mammal"""
     # TODO - implement this class!
 
+    # initializes panther class
+    def __init__(self, name):
+        super().__init__(name)
+        super().is_warm_blooded()
+        super().is_vertebrate()
+        self.size = 5
+        self.legs = 4
+        self.color = 'black'
 
-def size():
-    return 5
-
-
-def legs(self):
-    return 4
+    # how a panther calls
+    def call(self):
+        return "ROAR!"
 
