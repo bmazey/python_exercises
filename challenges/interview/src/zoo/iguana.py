e049094f7d7d8a9015d72d4a4552e9ffb008bead
serenawecker <swecker13@gmail.com>
2018-07-19 15:58:16 -0400

    iguana done!

--- challenges/interview/src/zoo/iguana.py
+++ challenges/interview/src/zoo/iguana.py
@@ -3,16 +3,22 @@ from challenges.interview.src.zoo.reptile import Reptile
 
 class Iguana(Reptile):
     """this is our public Iguana class, which 'extends' Reptile"""
-    size = 3
-    legs = 4
-    color = 'green'
 
     def __init__(self, name):
         super().__init__(name)
+        self.size = 3
+        self.legs = 4
+        self.color = 'green'
 
     # @override
     def eat(self):
         return "crunch ... crunch ..."
 
     def get_color(self):
-        return color
+        return self.color
+
+    def get_size(self):
+        return self.size
+
+    def get_legs(self):
+        return self.legs
