cfd62a36254538bd37fd276e7dd6afd87eda2d5f
DanBal332 <db3260@columbia.edu>
2018-07-19 14:31:14 -0400

    hi

--- challenges/interview/src/palindrome.py
+++ challenges/interview/src/palindrome.py
@@ -2,7 +2,28 @@ class Palindrome(object):
     """this is our palindrome class"""
 
     @staticmethod
-    def is_palindrome(s):
-        # TODO - implement this method!
+    # TODO - implement this method!
 
-        return True
+    def is_palindrome(s):
+        s = str(s)
+        a = s[::-1]
+        if a != s:
+            return False
+        else:
+            return True
+
+    print(is_palindrome)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
