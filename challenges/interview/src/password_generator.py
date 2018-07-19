93bfe6716503f3cf86076ac642830703ae5547e2
connie <conniezhuyx@gmail.com>
2018-07-19 13:55:36 -0400

    hahaha3

--- challenges/interview/src/password_generator.py
+++ challenges/interview/src/password_generator.py
@@ -13,10 +13,10 @@ class PasswordGenerator(object):
         symbols = '!@#$%^&*'
         password = ''
 
-        for i in range(4):
+        for i in range(0, 4):
             password += random.choice(string.ascii_letters)
 
-        for x in range(3):
+        for x in range(0, 3):
             password += str(random.randint(0, 9))
 
         password += str(random.choice(symbols))
