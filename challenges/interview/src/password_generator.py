119ba9a2bb44cea536549215006cda1f8a4b0a30
EvanCColumbia <ec3352@columbia.edu>
2018-07-19 15:33:36 -0400

    say a prayer 2.0

--- challenges/interview/src/password_generator.py
+++ challenges/interview/src/password_generator.py
@@ -6,7 +6,19 @@ class PasswordGenerator(object):
 
     @staticmethod
     def generate_password():
-        # TODO - implement this method!
 
         password = ''
+
+        nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+        letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        chars = ['!', '@', '#', '$', '%', '^', '&', '*']
+
+        for i in range(5):
+            password += str(letters[random.randint(0, 25)])
+
+        for i in range(4):
+            password += str(nums[random.randint(0, 9)])
+
+        password += str(chars[random.randint(0, 7)])
+
         return password
