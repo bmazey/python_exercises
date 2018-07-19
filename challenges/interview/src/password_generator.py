ad84fa0d467a51f71cda27c95750f2211c22021f
aj2852 <aj2852@columbia.edu>
2018-07-19 16:03:04 -0400

    test commit

--- challenges/interview/src/password_generator.py
+++ challenges/interview/src/password_generator.py
@@ -4,8 +4,14 @@ import random
 class PasswordGenerator(object):
     """this is a low quality PasswordGenerator"""
 
+    # creates a bank of characters to be used in the generate_password() function
     @staticmethod
     def generate_password():
-
+        letters = 'abcdefghijklmnopqrstuvwxyz'
+        numbers = '1234567890'
+        misc = '!@#$%^&*'
         password = ''
+        password = random.choice(letters) + random.choice(letters) + random.choice(letters) + random.choice(letters)\
+                    + random.choice(letters) + random.choice(numbers) + random.choice(numbers) + random.choice(numbers)\
+                    + random.choice(numbers) + random.choice(misc)
         return password
