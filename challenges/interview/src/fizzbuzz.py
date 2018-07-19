a32f30e96a372226754ebb9072e749cbf3b1a08b
Ferennie <ferennie.liu0119@gmail.com>
2018-07-19 15:11:25 -0400

    fizzbuzz code

--- challenges/interview/src/fizzbuzz.py
+++ challenges/interview/src/fizzbuzz.py
@@ -8,10 +8,13 @@ class FizzBuzz(object):
         # TODO - implement this method!
 
         string = ''
+        # if i is a multiple of 15, return fizzbuzz
         if i % 15 == 0:
             string = "fizzbuzz"
+        # if i is a multiple of 5, return buzz
         elif i % 5 == 0:
             string = "buzz"
+        # if i is a multiple of 3, return fizz
         elif i % 3 == 0:
             string = "fizz"
 
--- challenges/interview/src/zoo/panther.py
+++ challenges/interview/src/zoo/panther.py
@@ -5,3 +5,11 @@ class Panther(Mammal):
     """this is our Panther class which 'extends' Mammal"""
     # TODO - implement this class!
 
+
+def size():
+    return 5
+
+
+def legs(self):
+    return 4
+
