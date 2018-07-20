c9e64a96c6c5e26105d5d4e2ec38c0a09364353d
EvanCColumbia <ec3352@columbia.edu>
2018-07-20 14:18:33 -0400

    say a prayer 2.0

--- challenges/interview/src/zoo/iguana.py
+++ challenges/interview/src/zoo/iguana.py
@@ -3,4 +3,18 @@ from challenges.interview.src.zoo.reptile import Reptile
 
 class Iguana(Reptile):
     """this is our public Iguana class, which 'extends' Reptile"""
-    # TODO - implement this class!
+
+    def __init__(self, name):
+        super().__init__(name)
+        self.legs = 4
+        self.size = 3
+        self.color = 'green'
+
+    def get_size(self):
+        return self.size
+
+    def get_color(self):
+        return self.color
+
+    def get_legs(self):
+        return self.legs
