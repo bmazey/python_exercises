c94ca93a7bb820fcdaefa30af21bb64b801c1d6a
phoebecarolinemartin <pm3003@columbia.edu>
2018-07-23 16:26:33 -0400

    Merge remote-tracking branch 'remotes/origin/development' into phoebe
    # Conflicts:
    #	challenges/interview/src/fizzbuzz.py

--- exercises/structures/src/family_tree.py
+++ exercises/structures/src/family_tree.py
@@ -1,9 +1,5 @@
 
 import anytree
-from anytree
-import node
-render tree
-
 
 class FamilyTree:
 
@@ -12,19 +8,22 @@ class FamilyTree:
         self.george_oscar = anytree.Node("")
         self.michael = anytree.Node("")
         self.buster = anytree.Node("")
+        self.lindsay = anytree.Node("")
         self.george_michael = anytree.Node("")
         self.maeby = anytree.Node("")
 
-    def populate_family_tree():
-        lucille = anytree.Node("Lucille")
-        george_oscar = anytree.Node("George Oscar", parent=Lucille)
-        michael = anytree.Node("Lindsay", parent=Lucille_)
-        buster = anytree.Node("Buster", parent=Lucille)
-        george_michael = anytree.Node ("GeorgeMichael", parent=Michael)
-        maeby = anytree.Node("Maeby", parent=Lindsay)
+    def populate_family_tree(self):
+        self.lucille = anytree.Node("Lucille")
+        self.george_oscar = anytree.Node("George Oscar", parent=self.lucille)
+        self.lindsay = anytree.Node("Lindsay" , parent=self.lucille)
+        self.michael = anytree.Node("Michael", parent=self.lucille)
+        self.buster = anytree.Node("Buster", parent=self.lucille)
+        self.george_michael = anytree.Node ("GeorgeMichael", parent=self.michael)
+        self.maeby = anytree.Node("Maeby", parent=self.lindsay)
 
         # ex: child = Node('Child Name', parent = parent_node)
 
         # TODO - implement this method!
 
-        return
+        return self.lucille
+
