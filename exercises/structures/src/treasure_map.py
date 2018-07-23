5bbd17fa03c39c0e583d67497d19d0e378f47fb9
phoebecarolinemartin <pm3003@columbia.edu>
2018-07-23 16:12:32 -0400

    Merge remote-tracking branch 'remotes/origin/development' into phoebe
    # Conflicts:
    #	challenges/interview/src/fizzbuzz.py

--- exercises/structures/src/treasure_map.py
+++ exercises/structures/src/treasure_map.py
@@ -1,8 +1,9 @@
 class TreasureMap:
     def __init__(self):
-        self.map = {}
+        self.map = {'beach': 'sandy shore', 'coast': 'ocean reef', 'volcano': 'hot lava', 'x' :'marks the spot'}
 
     def populate_map(self):
+
 
         # TODO - implement this method!
         return
