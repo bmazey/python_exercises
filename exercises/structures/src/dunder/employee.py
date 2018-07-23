5797e361f7559fadfe33243d6de4e98847574279
connie <conniezhuyx@gmail.com>
2018-07-23 18:42:18 -0400

    5th

--- exercises/structures/src/dunder/employee.py
+++ exercises/structures/src/dunder/employee.py
@@ -2,5 +2,8 @@ class Employee(object):
     def __init__(self, employee_info, mail):
         self.employee_info = employee_info
         self._mail = mail
+        self.sender = employee_info.address
 
     # TODO - finish this class using dunder methods!
+    def __getitem__(self, item):
+        return self._mail[item]
