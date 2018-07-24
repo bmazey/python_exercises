4e3ff00c596f792d09f8e652b143e8195267d4a8
DanBal332 <db3260@columbia.edu>
2018-07-24 09:37:53 -0400

    hi

--- challenges/market/src/btcpricer.py
+++ challenges/market/src/btcpricer.py
@@ -13,25 +13,25 @@ class BtcPricer:
     @staticmethod
     def get_btc_usd_price_status():
         # TODO - implement this method!
-        req = requests.get('https://api.kraken.com/0/public/Ticker')
+        req = requests.post('https://api.kraken.com/0/public/Ticker')
         return req
 
     @staticmethod
     def get_btc_usd_price_response():
         # TODO - implement this method!
-        req = requests.get('https://api.kraken.com/0/public/Ticker').text
+        req = requests.post('https://api.kraken.com/0/public/Ticker').text
         return req
 
     @staticmethod
     def get_btc_usd_price():
         # TODO - implement this method!
-        price = requests.get('https://api.kraken.com/0/public/Ticker', params={'pair': 'XBTUSD'})
+        price = requests.post('https://api.kraken.com/0/public/Ticker', params={'pair': 'XBTUSD'})
         price = json.loads(price.text)['result']["XXBTZUSD"]["o"]
         return price
 
     @staticmethod
     def get_btc_cad_price():
         # TODO - implement this method!
-        price = requests.get('https://api.kraken.com/0/public/Ticker', params={'pair': 'XBTCAD'})
+        price = requests.post('https://api.kraken.com/0/public/Ticker', params={'pair': 'XBTCAD'})
         price = json.loads(price.text)['result']['XXBTZCAD']['o']
         return price
--- challenges/market/src/ethpricer.py
+++ challenges/market/src/ethpricer.py
@@ -13,26 +13,26 @@ class EthPricer:
     @staticmethod
     def get_eth_usd_price_status():
         # TODO - implement this method!
-        req = requests.get('https://api.kraken.com/0/public/Ticker')
+        req = requests.post('https://api.kraken.com/0/public/Ticker')
         return req
 
     @staticmethod
     def get_eth_usd_price_response():
         # TODO - implement this method!
-        req = requests.get('https://api.kraken.com/0/public/Ticker').text
+        req = requests.post('https://api.kraken.com/0/public/Ticker').text
         return req
 
     @staticmethod
     def get_eth_usd_price():
         # TODO - implement this method!
-        price = requests.get('https://api.kraken.com/0/public/Ticker', params={'pair': 'ETHUSD'})
+        price = requests.post('https://api.kraken.com/0/public/Ticker', params={'pair': 'ETHUSD'})
         price = json.loads(price.text)['result']['XETHZUSD']['o']
         return price
 
     @staticmethod
     def get_eth_cad_price():
         # TODO - implement this method!
-        price = requests.get('https://api.kraken.com/0/public/Ticker', params={'pair': 'ETHCAD'})
+        price = requests.post('https://api.kraken.com/0/public/Ticker', params={'pair': 'ETHCAD'})
         price = json.loads(price.text)['result']['XETHZCAD']['o']
         return price
 
