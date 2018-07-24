653c51a256d254bd943143e25f84191a31fc50a1
connie <conniezhuyx@gmail.com>
2018-07-24 14:02:42 -0400

    6th

--- challenges/market/src/ethpricer.py
+++ challenges/market/src/ethpricer.py
@@ -13,19 +13,22 @@ class EthPricer:
     @staticmethod
     def get_eth_usd_price_status():
         # TODO - implement this method!
-        return
+        eth = requests.get('https://api.kraken.com/0/public/Ticker')
+        return eth
 
     @staticmethod
     def get_eth_usd_price_response():
         # TODO - implement this method!
-        return
+        return requests.get('https://api.kraken.com/0/public/Ticker').text
 
     @staticmethod
     def get_eth_usd_price():
         # TODO - implement this method!
-        return
+        eth_usd = requests.get('https://api.kraken.com/0/public/Ticker', params={'pair': 'ETHUSD'})
+        return json.loads(eth_usd.text)['result']['XETHZUSD']['o']
 
     @staticmethod
     def get_eth_cad_price():
         # TODO - implement this method!
-        return
+        eth_cad = requests.get('https://api.kraken.com/0/public/Ticker', params={'pair': 'ETHCAD'})
+        return json.loads(eth_cad.text)['result']['XETHZCAD']['o']
