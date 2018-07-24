09cf785fb6419ef7073f2ba023ff377159183ea6
katienath10 <kn2464@columbia.edu>
2018-07-24 15:59:29 -0400

    tests passed

--- exercises/algorithms/src/rabin_karp.py
+++ exercises/algorithms/src/rabin_karp.py
@@ -9,8 +9,36 @@ d = 256
 # txt  -> text
 # q    -> A prime number
 
+# hash value for pattern
+# hash value for txt
 def search(pat, txt, q):
-    # TODO - implement this method!
-
+    m = len(pat)
+    n = len(txt)
+    j = 0
+    p = 0
+    t = 0
+    h = 1
     matches = list()
+
+    for i in range(m-1):
+        h = (h*d) % q
+    # calculate the has value of pattern and first window of text
+    for i in range(m):
+        p = (d*p + ord(pat[i])) % q
+        t = (d*t + ord(txt[i])) % q
+    # slide the pattern over text one by one
+    for i in range(n-m+1):
+        if p == t:
+            for j in range(m):
+                if txt[i+j] != pat[j]:
+                    break
+            j += 1
+            if j == m:
+                matches.append(i)
+
+        if i < n-m:
+            t = (d*(t-ord(txt[i])*h) + ord(txt[i+m])) % q
+            if t < 0:
+                t = t+q
+
     return matches
