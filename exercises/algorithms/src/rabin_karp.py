cb9d1de41c38f99ab636a565db7a7b1cc26cf6ea
Ferennie <ferennie.liu0119@gmail.com>
2018-07-24 16:08:43 -0400

    new variables

--- exercises/algorithms/src/rabin_karp.py
+++ exercises/algorithms/src/rabin_karp.py
@@ -11,48 +11,49 @@ d = 256
 
 def search(pat, txt, q):
     # TODO - implement this method!
-    m = len(pat)
-    n = len(txt)
+    pat_len = len(pat)
+    txt_len = len(txt)
     i = 0
     j = 0
-    p = 0  # hash value for pattern
-    t = 0  # hash value for txt
+    pat_hash = 0  # hash value for pattern
+    txt_hash = 0  # hash value for txt
     h = 1
     matches = list()
-    # The value of h would be "pow(d, M-1)%q"
-    for i in range(m - 1):
+
+    # The value of h would be "pow(d, pat_len-1)%q"
+    for i in range(pat_len - 1):
         h = (h * d) % q
 
     # Calculate the hash value of pattern and first window
     # of text
-    for i in range(m):
-        p = (d * p + ord(pat[i])) % q
-        t = (d * t + ord(txt[i])) % q
+    for i in range(pat_len):
+        pat_hash = (d * pat_hash + ord(pat[i])) % q
+        txt_hash = (d * txt_hash + ord(txt[i])) % q
 
     # Slide the pattern over text one by one
-    for i in range(n - m + 1):
+    for i in range(txt_len - pat_len + 1):
         # Check the hash values of current window of text and
         # pattern if the hash values match then only check
         # for characters on by one
-        if p == t:
+        if pat_hash == txt_hash:
             # Check for characters one by one
-            for j in range(m):
+            for j in range(pat_len):
                 if txt[i + j] != pat[j]:
                     break
 
             j += 1
-            # if p == t and pat[0...M-1] = txt[i, i+1, ...i+M-1]
-            if j == m:
+            # if pat_hash== txt_hashand pat[0...pat_len-1] = txt[i, i+1, ...i+pat_len-1]
+            if j == pat_len:
                 matches.append(i)
 
         # Calculate hash value for next window of text: Remove
         # leading digit, add trailing digit
-        if i < n - m:
-            t = (d * (t - ord(txt[i]) * h) + ord(txt[i + m])) % q
+        if i < txt_len - pat_len:
+            txt_hash= (d * (txt_hash- ord(txt[i]) * h) + ord(txt[i + pat_len])) % q
 
             # We might get negative values of t, converting it to
             # positive
-            if t < 0:
-                t = t + q
+            if txt_hash< 0:
+                txt_hash = txt_hash + q
 
     return matches
