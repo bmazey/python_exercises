f0bd433ee3ab1ad2936d3464c1df3a83051546f4
Ferennie <ferennie.liu0119@gmail.com>
2018-07-25 11:15:03 -0400

    Black Market Code

--- challenges/market/src/converter.py
+++ challenges/market/src/converter.py
@@ -1,9 +1,15 @@
 from challenges.market.src.btcpricer import BtcPricer
 from challenges.market.src.ethpricer import EthPricer
-
+import requests
+import json
 
 def main():
     # this is for experiments only! test does not look here ...
+    r = requests.get('https://api.kraken.com/0/public/Ticker?pair=XBTUSD')
+    mydata = json.loads(r.text)
+    price = mydata["result"]["XXBTZUSD"]["a"][0]
+    print(r)
+
     return
 
 
