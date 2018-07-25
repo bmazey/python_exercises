3d86a45b9253840a7ece5b735d90c495139beb13
gabriel_klass <gk2533@columbia.edu>
2018-07-25 15:23:40 -0400

    First three saves -- Done with fizzbuzz, palindrome, and password_generator

--- challenges/market/src/btcpricer.py
+++ challenges/market/src/btcpricer.py
@@ -12,20 +12,22 @@ class BtcPricer:
 
     @staticmethod
     def get_btc_usd_price_status():
-        # TODO - implement this method!
-        return
+        s = requests.get('https://api.kraken.com/0/public/Ticker')
+        return s
 
     @staticmethod
     def get_btc_usd_price_response():
-        # TODO - implement this method!
-        return
+        o = requests.get('https://api.kraken.com/0/public/Ticker').text
+        return o
 
     @staticmethod
     def get_btc_usd_price():
-        # TODO - implement this method!
-        return
+        price = requests.get('https://api.kraken.com/0/public/Ticker', params={'pair': 'XBTUSD'})
+        price = json.loads(price.text)['result']['XXBTZUSD']['o']
+        return price
 
     @staticmethod
     def get_btc_cad_price():
-        # TODO - implement this method!
-        return
+        price = requests.get('https://api.kraken.com/0/public/Ticker', params={'pair': 'XBTCAD'})
+        price = json.loads(price.text)['result']['XXBTZCAD']['o']
+        return price
