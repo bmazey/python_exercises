0c145af2ff4c1f8669b0627c7dd3eb2606820bd9
connie <conniezhuyx@gmail.com>
2018-07-26 16:22:31 -0400

    hieroglyph

--- exercises/expressions/src/hieroglyph.py
+++ exercises/expressions/src/hieroglyph.py
@@ -9,29 +9,37 @@ class Hieroglyph(object):
     @staticmethod
     def worship_sacred_cats(s):
         # TODO - implement this method!
-        return
+        match = re.match('(.)*cat(.)*', s)
+        return match
 
     @staticmethod
     def alphanumeric_glyph(s):
         # TODO - implement this method!
-        return
+        match = re.match(r'[a-z]{4}[0-9]{5,7}', s)
+        return match
 
     @staticmethod
     def avoid_nile_crocodile(s):
         # TODO - implement this method!
-        return
+        search = re.search(r'crocodile', s)
+        return not search
 
     @staticmethod
     def find_gold_scarab(s):
         # TODO - implement this method!
-        return
+        if s == 'gold scarab':
+            return 'gold'
+        else:
+            return False
 
     @staticmethod
     def raid_tuts_tomb(s):
         # TODO - implement this method!
-        return
+        search = re.search(r'tut', s.casefold())
+        return not search
 
     @staticmethod
     def steal_crystal_skull(s):
         # TODO - implement this method!
-        return
+        sub = re.sub(r'skull', 'idol', s)
+        return sub
