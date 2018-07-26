936cfad4a239322061dae6036b922ef1088c3679
Ferennie <ferennie.liu0119@gmail.com>
2018-07-26 16:06:34 -0400

    hieroglyph code

--- exercises/expressions/src/hieroglyph.py
+++ exercises/expressions/src/hieroglyph.py
@@ -8,30 +8,40 @@ class Hieroglyph(object):
 
     @staticmethod
     def worship_sacred_cats(s):
-        # TODO - implement this method!
-        return
+        # checks if string contains 'cat'
+        match = re.match('(.*)(cat*)(.*)', s)
+        return match
 
     @staticmethod
     def alphanumeric_glyph(s):
-        # TODO - implement this method!
-        return
+        # checks if string contains 4 letters followed with at least one number
+        match = re.match('[a-zA-Z]{4}([0-9]+)', s)
+        return match
 
     @staticmethod
     def avoid_nile_crocodile(s):
-        # TODO - implement this method!
-        return
+        # checks if you find one or more 'crocodile' in the string
+        # if match= true, returns !match
+        match = re.search('(crocodile+)', s)
+        return not match
 
     @staticmethod
     def find_gold_scarab(s):
-        # TODO - implement this method!
-        return
+        # checks if string is exactly 'gold scarab'
+        match = re.match('(gold scarab)', s)
+        if match:
+            return 'gold'
 
     @staticmethod
     def raid_tuts_tomb(s):
-        # TODO - implement this method!
-        return
+        # searches for keyword 'tut' ignoring case
+        # if 'tut' appears, return !match
+        match = re.search('(tut)', s, re.IGNORECASE)
+        return not match
 
     @staticmethod
     def steal_crystal_skull(s):
-        # TODO - implement this method!
-        return
+        # searches for 'skull' and replaces with 'idol'
+        # returns substituted version of string
+        match = re.sub('(skull)', 'idol', s)
+        return match
