ba8fa9cc82d0d52cb779c3887f70e85cbb13ed9c
s <shoutianze@outlook.com>
2018-07-26 15:54:14 -0400

    test push

--- exercises/expressions/src/hieroglyph.py
+++ exercises/expressions/src/hieroglyph.py
@@ -9,29 +9,33 @@ class Hieroglyph(object):
     @staticmethod
     def worship_sacred_cats(s):
         # TODO - implement this method!
-        return
+        result = re.search('cat', s)
+        return result
 
     @staticmethod
     def alphanumeric_glyph(s):
         # TODO - implement this method!
-        return
+        return re.search(r'[a-z]{4}[0-9]+', s)
 
     @staticmethod
     def avoid_nile_crocodile(s):
         # TODO - implement this method!
-        return
+        return not re.search('crocodile', s)
 
     @staticmethod
     def find_gold_scarab(s):
         # TODO - implement this method!
-        return
+        if s == 'gold scarab':
+            return 'gold'
+        else:
+            return False
 
     @staticmethod
     def raid_tuts_tomb(s):
         # TODO - implement this method!
-        return
+        return not re.search('tut', s.casefold())
 
     @staticmethod
     def steal_crystal_skull(s):
         # TODO - implement this method!
-        return
+        return re.sub('skull', 'idol', s)
