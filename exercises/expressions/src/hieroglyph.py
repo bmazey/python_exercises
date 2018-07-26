017c79eeddb832975436406242a3f522538758fc
aj2852 <aj2852@columbia.edu>
2018-07-26 16:43:23 -0400

    test commit

--- exercises/expressions/src/hieroglyph.py
+++ exercises/expressions/src/hieroglyph.py
@@ -9,29 +9,35 @@ class Hieroglyph(object):
     @staticmethod
     def worship_sacred_cats(s):
         # TODO - implement this method!
-        return
+        cat = re.search('cat', s)
+        return cat
 
     @staticmethod
     def alphanumeric_glyph(s):
         # TODO - implement this method!
-        return
+        glyph = re.search(r'[a-z]{4}[0-9]+', s)
+        return glyph
 
     @staticmethod
     def avoid_nile_crocodile(s):
         # TODO - implement this method!
-        return
+        croc = re.search('crocodile', s)
+        return not croc
 
     @staticmethod
     def find_gold_scarab(s):
         # TODO - implement this method!
-        return
+        gold = re.search('gold scarab', s)
+        if gold:
+            return 'gold'
 
     @staticmethod
     def raid_tuts_tomb(s):
         # TODO - implement this method!
-        return
+        return not re.search('tut', s.casefold())
 
     @staticmethod
     def steal_crystal_skull(s):
         # TODO - implement this method!
-        return
+        skull = (re.sub('skull', 'idol', s))
+        return skull
