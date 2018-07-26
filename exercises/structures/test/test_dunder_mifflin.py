b3a1cca6e8bb62b335fcaf4616de3d8084d3fb94
alyssahwang <ahh2143@columbia.edu>
2018-07-26 15:41:50 -0400

    add test comment to test_dunder_mifflin.py

--- exercises/structures/test/test_dunder_mifflin.py
+++ exercises/structures/test/test_dunder_mifflin.py
@@ -6,6 +6,7 @@ from exercises.structures.src.dunder.employee_info import EmployeeInfo
 
 Email = namedtuple('Email', 'sender recipient message replies')
 
+# test test i should be able to see this
 
 # you'll need to look here: https://www.python-course.eu/python3_magic_methods.php
 def test_office():
