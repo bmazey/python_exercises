3d3345638e58700b6059adaf92581b79685c33f2
Aboodi <a.aboodi@columbia.edu>
2018-07-26 14:43:14 -0400

    Office Complete

--- exercises/structures/src/dunder/employee.py
+++ exercises/structures/src/dunder/employee.py
@@ -2,5 +2,9 @@ class Employee(object):
     def __init__(self, employee_info, mail):
         self.employee_info = employee_info
         self._mail = mail
+        self.sender = employee_info.address
 
     # TODO - finish this class using dunder methods!
+
+    def __getitem__(self, position):
+        return self._mail[position]
--- exercises/structures/src/dunder/office.py
+++ exercises/structures/src/dunder/office.py
@@ -2,5 +2,15 @@ class Office(object):
     def __init__(self, employees, boss=None):
         self.employees = employees
         self.boss = boss
+
     # TODO - finish class using dunder methods!
 
+    def __getitem__(self, position):
+        return self.employees[position]
+
+    def __add__(self, other):
+        total = self.employees + (other,)
+        return Office(total, self.boss)
+
+    def __len__(self):
+        return len(self.employees)
