80f48c2a6c79e8d18f7ef70c74adb3085c7af4eb
gabriel_klass <gk2533@columbia.edu>
2018-07-26 22:25:37 -0400

    Merge remote-tracking branch 'remotes/origin/brandon-expressions' into gabe-market
    # Conflicts:
    #	exercises/patterns/builder/car_builder_director.py

--- exercises/patterns/builder/car_builder_director.py
+++ exercises/patterns/builder/car_builder_director.py
@@ -5,6 +5,7 @@ class CarBuilderDirector(object):
 
     @staticmethod
     def construct_suv():
+        # creates yellow suv with 4 wheels 4 doors and unleaded gas
         suv = exercises.patterns.builder.car_builder.CarBuilder()
         suv.set_wheels(4)
         suv.set_doors(4)
@@ -14,6 +15,7 @@ class CarBuilderDirector(object):
 
     @staticmethod
     def construct_sports_car():
+        # creates red sports car with 4 wheels 2 doors and premium gas
         sc = exercises.patterns.builder.car_builder.CarBuilder()
         sc.set_wheels(4)
         sc.set_doors(2)
@@ -23,6 +25,7 @@ class CarBuilderDirector(object):
 
     @staticmethod
     def construct_eighteen_wheeler():
+        # creates blue eighteen wheeler with 18 wheels 2 doors and diesel
         ew = exercises.patterns.builder.car_builder.CarBuilder()
         ew.set_wheels(18)
         ew.set_doors(2)
@@ -32,6 +35,7 @@ class CarBuilderDirector(object):
 
     @staticmethod
     def construct_tesla():
+        # creates black tesla with 4 wheels 4 doors and electricity
         tes = exercises.patterns.builder.car_builder.CarBuilder()
         tes.set_wheels(4)
         tes.set_doors(4)
