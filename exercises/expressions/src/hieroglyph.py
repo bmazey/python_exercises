d3caf3d324d1e325414d186e71b373255813a620
gabriel_klass <gk2533@columbia.edu>
2018-07-26 22:22:43 -0400

    Merge remote-tracking branch 'remotes/origin/brandon-expressions' into gabe-market
    # Conflicts:
    #	exercises/patterns/builder/car_builder_director.py

--- exercises/expressions/src/hieroglyph.py
+++ exercises/expressions/src/hieroglyph.py
@@ -8,30 +8,33 @@ class Hieroglyph(object):
 
     @staticmethod
     def worship_sacred_cats(s):
-        # TODO - implement this method!
-        return
+        # returns true if it includes the word cat
+        return re.match(r'.*cat+.*', s, re.I)
 
     @staticmethod
     def alphanumeric_glyph(s):
-        # TODO - implement this method!
-        return
+        # returns true if 4 letters and then 5 or 7 numbers
+        return re.match(r'[a-z]{4}[0-9]{5,7}', s, re.I)
 
     @staticmethod
     def avoid_nile_crocodile(s):
-        # TODO - implement this method!
-        return
+        # returns true if no existence of the word crocodile
+        return not re.search(r'crocodile', s)
 
     @staticmethod
     def find_gold_scarab(s):
-        # TODO - implement this method!
-        return
+        # returns string 'gold' if s only possesses 'gold's and 'scarab's in that order. Else, false
+        if re.match(r'gold+ scarab+', s):
+            return 'gold'
+        else:
+            return False
 
     @staticmethod
     def raid_tuts_tomb(s):
-        # TODO - implement this method!
-        return
+        # returns true when s has no existence of the word 'tut'
+        return not re.search(r'tut', s, re.I)
 
     @staticmethod
     def steal_crystal_skull(s):
-        # TODO - implement this method!
-        return
+        # replaces all instances of the word skull with idol
+        return re.sub(r'skull', 'idol', s)
