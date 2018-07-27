d46200a41b730443b19e9891e6d6d886c694eea0
serenawecker <swecker13@gmail.com>
2018-07-27 11:56:13 -0400

    black market done

--- exercises/structures/src/dunder/office.py
+++ exercises/structures/src/dunder/office.py
@@ -7,4 +7,7 @@ class Office(object):
         return len(self.employees)
 
     def __add__(self, other):
-        self.employees + other
+        return Office(self.employees + (other,), self.boss)
+
+    def __getitem__(self, item):
+        return self.employees[item]
