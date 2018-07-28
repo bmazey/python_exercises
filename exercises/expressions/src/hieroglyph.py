59f53b5d17263634356898fc96c65244e98617ac
Alan <asun17904@gmail.com>
2018-07-28 11:17:11 -0400

    regex

--- exercises/expressions/src/hieroglyph.py
+++ exercises/expressions/src/hieroglyph.py
@@ -9,29 +9,29 @@ class Hieroglyph(object):
     @staticmethod
     def worship_sacred_cats(s):
         # TODO - implement this method!
-        return
+        return re.match(r'.*cat*',s)
 
     @staticmethod
     def alphanumeric_glyph(s):
         # TODO - implement this method!
-        return
+        return re.match(r'[a-z]{4}[0-9]{5,7}',s)
 
     @staticmethod
     def avoid_nile_crocodile(s):
-        # TODO - implement this method!
-        return
+        return not re.search(r'crocodile',s)
+
 
     @staticmethod
     def find_gold_scarab(s):
         # TODO - implement this method!
-        return
+        return re.search(r'(gold) (scarab)',s).group(1) if re.search(r'(gold) (scarab)',s) else False
 
     @staticmethod
     def raid_tuts_tomb(s):
         # TODO - implement this method!
-        return
+        return not re.search('tut',s,re.I)
 
     @staticmethod
     def steal_crystal_skull(s):
         # TODO - implement this method!
-        return
+        return re.sub(r'skull','idol',s)
